149b79c7580be2fc423f739943d17c1c5b4c0fdd
Unknown <mutsaddi@gmail.com>
2018-09-17 10:39:56 +0530

    routing 1.1

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
   const routes: Routes = [
-  { path: ' ',
-    redirectTo: 'home',
+  { path: '',
+    redirectTo: 'login',
     pathMatch: 'full'
 
   },
@@ -13,6 +14,10 @@ import { HomeComponent } from './home/home.component';
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
